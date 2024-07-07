8836eb3d4543f572ee57e8c2841b9538e56e1852
009661@#m <tari.almutairi1995@gmai.com>
2024-07-07 23:43:52 +0300

    abb

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -20,19 +20,19 @@ function Homepage() {
                   <p className=' text-xl text-gray-400'>Mountains</p>
                 </div>
                 <div className='flex flex-col items-center m-2'>
-                <a href="/Seass"><div className='w-24 h-24 bg-slate-300 rounded-full shadow-2xl mb-10 cursor-pointer bg-[url("https://images.pexels.com/photos/1544376/pexels-photo-1544376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] bg-cover bg-center ease-in duration-300 hover:mb-12' ></div></a>
+                 <Link to={"/Seass"}><div className='w-24 h-24 bg-slate-300 rounded-full shadow-2xl mb-10 cursor-pointer bg-[url("https://images.pexels.com/photos/1544376/pexels-photo-1544376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] bg-cover bg-center ease-in duration-300 hover:mb-12' ></div></Link>
                   <p className='text-xl  text-gray-400'>Seas</p>
                 </div>
                 <div className='flex flex-col items-center m-2'>
-                <a href="/Desertss"><div className='w-24 h-24 bg-slate-300 rounded-full shadow-2xl mb-14 cursor-pointer bg-[url("https://images.pexels.com/photos/4608979/pexels-photo-4608979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] bg-cover bg-center ease-in duration-300 hover:mb-16'></div></a>
+                 <Link to={"/Desertss"}><div className='w-24 h-24 bg-slate-300 rounded-full shadow-2xl mb-14 cursor-pointer bg-[url("https://images.pexels.com/photos/4608979/pexels-photo-4608979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] bg-cover bg-center ease-in duration-300 hover:mb-16'></div></Link>
                   <p className=' text-xl text-gray-400'>Deserts</p>
                 </div>
                 <div className='flex flex-col items-center m-2'>
-                <a href="/Plainss"><div className='w-24 h-24 bg-slate-300 rounded-full shadow-2xl mb-10 cursor-pointer bg-[url("https://images.pexels.com/photos/15986262/pexels-photo-15986262/free-photo-of-clouds-over-transmission-towers-on-fields.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] bg-cover bg-center ease-in duration-300 hover:mb-12' ></div></a>
+                 <Link to={"/Plainss"}><div className='w-24 h-24 bg-slate-300 rounded-full shadow-2xl mb-10 cursor-pointer bg-[url("https://images.pexels.com/photos/15986262/pexels-photo-15986262/free-photo-of-clouds-over-transmission-towers-on-fields.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] bg-cover bg-center ease-in duration-300 hover:mb-12' ></div></Link>
                   <p className=' text-xl text-gray-400'>Plains</p>
                 </div>
                 <div className='flex flex-col items-center m-2'>
-                <a href="/Forestss"><div className='w-24 h-24 bg-slate-300 rounded-full shadow-2xl mb-5 cursor-pointer bg-[url("https://images.pexels.com/photos/3801347/pexels-photo-3801347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] bg-cover bg-center ease-in duration-300 hover:mb-7' ></div></a>
+                 <Link to={"/Forestss"}><div className='w-24 h-24 bg-slate-300 rounded-full shadow-2xl mb-5 cursor-pointer bg-[url("https://images.pexels.com/photos/3801347/pexels-photo-3801347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")] bg-cover bg-center ease-in duration-300 hover:mb-7' ></div></Link>
                   <p className=' text-xl text-gray-400'>Forests</p>
                 </div>
               </div>
